f4a3727c10b8bc23255ddc714619e2d39788081e
Bruno Dorea <brunodorea@outlook.com.br>
2022-10-20 15:32:43 -0300

    commit parte 1
    1:41:19

---
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
@@ -0,0 +1,50 @@
+package me.dio.copa.catar.features
+
+import android.content.res.Resources.NotFoundException
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import me.dio.copa.catar.core.BaseViewModel
+import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.usecase.GetMatchesUseCase
+import me.dio.copa.catar.remote.UnexpectedException
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getMatchesUseCase: GetMatchesUseCase,
+) : BaseViewModel<MainUiState, MainUiAction>(MainUiState()) {
+
+    init {
+        fetchMatches()
+    }
+
+    private fun fetchMatches() = viewModelScope.launch {
+        getMatchesUseCase()
+            .flowOn(Dispatchers.Main)
+            .catch {
+                when(it) {
+                    is NotFoundException ->
+                        sendAction(MainUiAction.MatchesNotFound(it.message ?: "Erro sem mensagem"))
+                    is UnexpectedException ->
+                        sendAction(MainUiAction.Unexpected)
+                }
+            }.collect { matches ->
+                setState {
+                    copy(matches = matches)
+                }
+            }
+    }
+}
+
+data class MainUiState(
+    val matches: List<MatchDomain> = emptyList()
+)
+
+sealed class MainUiAction {
+    data class MatchesNotFound(val message: String) : MainUiAction()
+    object Unexpected: MainUiAction()
+}
